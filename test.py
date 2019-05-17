4544bb9a2c44c2cdc71e6017503a6a126b1ab962
Julio Raygoza <monk16biit@gmail.com>
2019-05-17 15:41:58 -0700

    Django Comment

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 print('Test 0')
 
 print('Change was made')
+
+# All is going well finally.
+# Not sure if I should redo entire Django App
+# On here or just get the current app up and running.
+
+# What do you think?
